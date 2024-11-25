ee7373d956cf0ae60bbfd4c1850f863e739805a6
Leonardo Da Vinci <84631921+leanardo7994@users.noreply.github.com>
2024-11-25 10:45:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>Hello {first} 👋</b>\n\n<b>I Can Store</b> 𝐌𝐎𝐕𝐈𝐄𝐒 𝐄𝐌𝐏𝐎𝐑𝐈𝐎 <b>Files In This Bot And Other Users Can Access It From Special Link 🔗</b>\n\n<blockquote><b><a href='https://t.me/movie_emporio'>BEFORE THAT YOU NEED TO JOIN IN OUR CHANNEL TO DOWNLOAD THE MOVIE FILES 📂</a></b></blockquote>\n\n<b>After Join The Channel Click The 🔄 TRY AGAIN Button To Get Files</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>Hello {first} 👋</b>\n<blockquote><b>I Can Store</b> 𝐌𝐎𝐕𝐈𝐄𝐒 𝐄𝐌𝐏𝐎𝐑𝐈𝐎 <b>Files In This Bot And Other Users Can Access It From Special Link 🔗</b></blockquote>\n<blockquote><b><a href='https://t.me/movie_emporio'>BEFORE THAT YOU NEED TO JOIN IN OUR CHANNEL TO DOWNLOAD THE MOVIE FILES 📂</a></b></blockquote>\n\n<b>After Join The Channel Click The 🔄 TRY AGAIN Button To Get The Files</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
